aad046d1757cc81bcf3ec3c7ce5c1fc989248c16
Aaron Stevens <bheklilr2@gmail.com>
2018-03-22 15:58:15 -0500

    Now using ast module to do basic parsing

--- workshop1/pycalc/pycalc/calculator.py
+++ workshop1/pycalc/pycalc/calculator.py
@@ -1,18 +1,28 @@
-
-class Expression:
-    pass
+import ast
 
 
 class Calculator:
     def __init__(self) -> None:
         pass
 
-    def parse(self, expression: str) -> Expression:
-        pass
-
-    def evaluate(self, expr: Expression) -> float:
-        pass
+    def parse(self, expression: str) -> ast.Expr:
+        module: ast.Module = ast.parse(expression)
+        body: ast.Expr = module.body[0].value
+        return body
 
     def calculate(self, expression: str) -> float:
         expr = self.parse(expression)
-        return self.evaluate(expr)
+        reduced = self.evaluate(expr)
+        return reduced
+
+    def evaluate(self, expr: ast.AST) -> float:
+        if isinstance(expr, ast.Num):
+            return expr.n
+
+        if isinstance(expr, ast.BinOp):
+            if type(expr.op) is ast.Add:
+                left = self.evaluate(expr.left)
+                right = self.evaluate(expr.right)
+                return left + right
+
+        raise TypeError('Unsupported expression')
