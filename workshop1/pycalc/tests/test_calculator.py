58a0099bb6322be80691b77ff51e2db968aaff9e
Aaron Stevens <bheklilr2@gmail.com>
2018-03-22 16:11:38 -0500

    Added failing tests for mult and div

--- workshop1/pycalc/tests/test_calculator.py
+++ workshop1/pycalc/tests/test_calculator.py
@@ -10,3 +10,15 @@ def calc():
 
 def test_simple_arithmetic_add(calc):
     assert calc.calculate('2 + 2') == 4
+
+
+def test_simple_arithmetic_subtract(calc):
+    assert calc.calculate('2 - 1') == 1
+
+
+def test_simple_arithmetic_multiply(calc):
+    assert calc.calculate('3 * 4') == 12
+
+
+def test_simple_arithmetic_divide(calc):
+    assert calc.calculate('10 / 3') == (10 / 3)
