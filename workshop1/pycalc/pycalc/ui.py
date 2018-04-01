bf38c9e6bfa499d55d3669968fd4acb8e2a6ec56
Aaron Stevens <bheklilr2@gmail.com>
2018-03-31 23:15:45 -0500

    Added event handler for numbers and ops

--- workshop1/pycalc/pycalc/ui.py
+++ workshop1/pycalc/pycalc/ui.py
@@ -33,16 +33,33 @@ class PyCalcUI(tk.Frame):
             for j in range(3):
                 number = 3 * i + j + 1
                 row = 3 - i
-                self.btns[number] = tk.Button(self, text=str(number))
+                self.btns[number] = tk.Button(
+                    self,
+                    text=str(number),
+                    command=self.on_number_clicked(number),
+                )
                 self.btns[number].grid(row=row, column=j, **opts)
         self.btns[0] = tk.Button(self, text='0')
         self.btns[0].grid(row=4, column=0, columnspan=2, **opts)
 
         self.op_btns = {}
 
-        self.op_btns['='] = tk.Button(self, text='=')
+        self.op_btns['='] = tk.Button(self, text='=', command=self.on_op_clicked('='))
         self.op_btns['='].grid(row=4, column=2, **opts)
 
         for i, op in enumerate(Calculator.SUPPORTED_OPERATORS, 1):
-            self.op_btns[op] = tk.Button(self, text=op)
+            self.op_btns[op] = tk.Button(self, text=op, command=self.on_op_clicked(op))
             self.op_btns[op].grid(row=i, column=3, **opts)
+
+    def on_number_clicked(self, number):
+        def on_clicked():
+            current = self.entry_var.get()
+            if current == '0':
+                current = ''
+            self.entry_var.set(current + str(number))
+        return on_clicked
+
+    def on_op_clicked(self, op):
+        def on_clicked():
+            pass
+        return on_clicked
