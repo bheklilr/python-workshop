3b0ec02d6e91f6c9d93e9783fb667b46f0fa8c69
Aaron Stevens <bheklilr2@gmail.com>
2018-03-31 23:34:36 -0500

    Corrected several stylistic and type annotation issues

--- workshop1/pycalc/pycalc/ui.py
+++ workshop1/pycalc/pycalc/ui.py
@@ -1,3 +1,4 @@
+from typing import Dict, Callable
 import tkinter as tk
 from .calculator import Calculator  # noqa
 
@@ -20,15 +21,16 @@ class PyCalcUI(tk.Frame):
 
     def create_widgets(self) -> None:
         self.entry_var = tk.StringVar(self, value='0')
-        self.entry = tk.Entry(self, textvariable=self.entry_var, justify=tk.RIGHT)
+        self.entry = tk.Entry(self, textvariable=self.entry_var)
+        self.entry['justify'] = tk.RIGHT
         self.entry.grid(row=0, column=0, columnspan=4)
 
         opts = {
-            'sticky':tk.N + tk.S + tk.E + tk.W,
+            'sticky': tk.N + tk.S + tk.E + tk.W,
             'padx': 2,
             'pady': 2,
         }
-        self.btns = {}
+        self.btns: Dict[int, tk.Button] = {}
         for i in range(3):
             for j in range(3):
                 number = 3 * i + j + 1
@@ -42,24 +44,26 @@ class PyCalcUI(tk.Frame):
         self.btns[0] = tk.Button(self, text='0')
         self.btns[0].grid(row=4, column=0, columnspan=2, **opts)
 
-        self.op_btns = {}
+        self.op_btns: Dict[str, tk.Button] = {}
 
-        self.op_btns['='] = tk.Button(self, text='=', command=self.on_op_clicked('='))
+        self.op_btns['='] = tk.Button(self, text='=',
+                                      command=self.on_op_clicked('='))
         self.op_btns['='].grid(row=4, column=2, **opts)
 
         for i, op in enumerate(Calculator.SUPPORTED_OPERATORS, 1):
-            self.op_btns[op] = tk.Button(self, text=op, command=self.on_op_clicked(op))
+            self.op_btns[op] = tk.Button(self, text=op,
+                                         command=self.on_op_clicked(op))
             self.op_btns[op].grid(row=i, column=3, **opts)
 
-    def on_number_clicked(self, number):
-        def on_clicked():
+    def on_number_clicked(self, number: int) -> Callable[[], None]:
+        def on_clicked() -> None:
             current = self.entry_var.get()
             if current == '0':
                 current = ''
             self.entry_var.set(current + str(number))
         return on_clicked
 
-    def on_op_clicked(self, op):
-        def on_clicked():
+    def on_op_clicked(self, op: str) -> Callable[[], None]:
+        def on_clicked() -> None:
             pass
         return on_clicked
