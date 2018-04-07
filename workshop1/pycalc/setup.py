1375090e4466a85bdad64af875d3df20ac1900ed
Aaron Stevens <bheklilr2@gmail.com>
2018-04-07 10:33:33 -0500

    Added some docstrings and such in setup.py.  Refactored CheckCommand.run into three methods

--- workshop1/pycalc/setup.py
+++ workshop1/pycalc/setup.py
@@ -1,22 +1,50 @@
 from setuptools import setup, find_packages
 from setuptools import Command
 from pkg_resources import EntryPoint, Distribution
+import subprocess as sp
 
 version = '1.0.0'
 
 
 class CheckCommand(Command):
+    """This is a custom setup.py command that runs multiple code quality tools.
+    This class is passed to the `setup` method and is used to extend the default
+    commands for setup.py.  To find out what all commands are available, try
+    running `python setup.py --help-commands`.
+
+    It works by inheriting from `setuptools.Command` and overriding the `run()`
+    method.  The `run` method is where the logic for this command is defined.
+
+    Because the `Command` class expects there to be arguments most of the time,
+    there are two other methods, `initialize_options` and `finalize_options`
+    that must also be defined for this class to work, even if they do nothing.
+
+    Right now, the tools that are run are:
+
+      * flake8 - A style checking tool. This checks for problems like lines that
+                 are too long, variable names that don't follow the same
+                 convention, and common problems that could become bugs.
+      * mypy - A static type checker that takes advantage of the new type
+               annotations in Python 3.6
+      * pytest - A powerful unit testing framework. Testing code is important to
+                 ensure correctness and robustness of code.
+    """
+
+    #: This class-level variable is where you would place any additional
+    #: arguments if you needed them.
     user_options = []
 
     def initialize_options(self):
+        """The `Command` class requires that this method is defined."""
         pass
 
     def finalize_options(self):
-        pass
+        """The `Command` class requires that this method is defined as well. Did
+        you know that if you have a docstring in an empty method you don't need
+        a `pass` statement?
+        """
 
-    def run(self):
-        import subprocess as sp
-
+    def run_flake8(self):
         try:
             import flake8  # noqa
         except ImportError:
@@ -29,6 +57,7 @@ class CheckCommand(Command):
                 for line in output.split('\n'):
                     self.announce('\t' + line, level=3)
 
+    def run_mypy(self):
         try:
             import mypy  # noqa
         except ImportError:
@@ -41,12 +70,18 @@ class CheckCommand(Command):
                 for line in output.split('\n'):
                     self.announce('\t' + line, level=3)
 
+    def run_pytest(self):
         try:
             import pytest
         except ImportError:
             self.warn('pytest is not installed, skipping tests.')
         else:
             pytest.cmdline.main([])
+
+    def run(self):
+        self.run_flake8()
+        self.run_mypy()
+        self.run_pytest()
 
 
 class RunCommand(Command):
